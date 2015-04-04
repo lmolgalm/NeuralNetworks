555c03a16e38839b586103d5f40dc34f0c07ec37
IvanStelmakh <stelmakh@MBP-stelmakh.campus.mipt.ru>
2015-04-04 18:56:57 +0300

    cleaned up

--- audio_graph.py
+++ audio_graph.py
@@ -1,14 +1,19 @@
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+import matplotlib.ticker as ticker #for what???
 import math
+
+# create dictionary(this step might be ommited if we will use
+# only one type of audiofile)
 
 types = {
     1: np.int8,
     2: np.int16,
     4: np.int32
 }
+
+# definig functions to format graph
 
 def format_time(x, pos=None):
     global duration, nframes, k
@@ -26,14 +31,16 @@ def format_db(x, pos=None):
     global peak
     if x == 0:
         return "-inf"
-    
     db = 20 * math.log10(abs(x) / float(peak))
     return int(db)
 
+# open file(it may be better to combine two programs into one)
 wav = wave.open("output.wav", mode="r")
+#(number of channels, byte per sample, frame per second, number of frames, compression type, compression name)
 (nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
 
 duration = nframes / framerate
+# paremeters of graph
 w, h = 800, 300
 k = nframes/w/32
 DPI = 72
@@ -42,8 +49,13 @@ peak = 256 ** sampwidth / 2
 content = wav.readframes(nframes)
 samples = np.fromstring(content, dtype=types[sampwidth])
 
+f=open("test.txt", 'wb')
+np.savetxt(f, samples, '%10.1f', newline="")
+
 plt.figure(1, figsize=(float(w)/DPI, float(h)/DPI), dpi=DPI)
 plt.subplots_adjust(wspace=0, hspace=0)
+
+# now we are making graph
 
 for n in range(nchannels):
     channel = samples[n::nchannels]
--- catching_audio.py
+++ catching_audio.py
@@ -1,14 +1,18 @@
 import pyaudio
 import wave
 
+# here we declare parameters of recording
+
 CHUNK = 1024 
-FORMAT = pyaudio.paInt16 #paInt8
-CHANNELS = 2 
-RATE = 44100 #sample rate
+FORMAT = pyaudio.paInt16   #16bit
+CHANNELS = 1               #mono sound
+RATE = 44100               #sample rate
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
+
+#use class for audio
 
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
@@ -16,9 +20,16 @@ stream = p.open(format=FORMAT,
                 input=True,
                 frames_per_buffer=CHUNK) #buffer
 
+#srat recording
+
 print("* recording")
 
+# the empty frame(list) is generated where recorded info will be stored
+
 frames = []
+
+#here we read CHUNK of data as many times as we need and
+#store it into our frame(list)
 
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
@@ -26,9 +37,13 @@ for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 
 print("* done recording")
 
+#stop recording
+
 stream.stop_stream()
 stream.close()
 p.terminate()
+
+# write sound into new file(possibly, this step might be ommited)
 
 wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 wf.setnchannels(CHANNELS)
